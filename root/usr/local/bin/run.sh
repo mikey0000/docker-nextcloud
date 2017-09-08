46ba5d181201c3747d6ec9dc49925863355d8c92
Michael Arthur <michael@jumblesoft.co.nz>
2017-09-08 19:47:02 +1200

    Update run.sh

--- root/usr/local/bin/run.sh
+++ root/usr/local/bin/run.sh
@@ -30,7 +30,7 @@ if [ ! -d /data/session ]; then
 fi
 
 echo "Updating permissions..."
-for dir in /nextcloud /data /config /apps2 /etc/nginx /etc/php7 /var/log /var/lib/nginx /tmp /etc/s6.d; do
+for dir in /opt/nextcloud /data /config /apps2 /etc/nginx /var/log /var/lib/nginx /tmp /etc/s6.d; do
   if $(find $dir ! -user $UID -o ! -group $GID|egrep '.' -q); then
     echo "Updating permissions in $dir..."
     chown -R $UID:$GID $dir
